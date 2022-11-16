4544fc604f3f8a7c6e1c3e2ba128ae6ad7ac49f8
Saaideh-Osama <saaidehosama@gmail.com>
2022-11-16 12:50:43 +0200

    last

---
+++ react-php-login-master/src/components/Register.js
@@ -0,0 +1,77 @@
+import {useContext, useState} from 'react'
+import {Link} from 'react-router-dom'
+import {UserContext} from '../context/UserContext';
+
+const Register = () => {
+    const {registerUser, wait} = useContext(UserContext);
+    const [errMsg, setErrMsg] = useState(false);
+    const [successMsg, setSuccessMsg] = useState(false);
+    const [formData, setFormData] = useState({
+        name:'',
+        email:'',
+        password:'',
+        department:''
+    });
+console.log(formData);
+    const onChangeInput = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name]:e.target.value
+        })
+    }
+
+    const submitForm = async (e) => {
+        e.preventDefault();
+
+        if(!Object.values(formData).every(val => val.trim() !== '')){
+            setSuccessMsg(false);
+            setErrMsg('Please Fill in all Required Fields!');
+            return;
+        }
+
+        const data = await registerUser(formData);
+        if(data.success){
+            e.target.reset();
+            setSuccessMsg('You have successfully registered.');
+            setErrMsg(false);
+        }
+        else if(!data.success && data.message){
+            setSuccessMsg(false);
+            setErrMsg(data.message);
+        }
+        
+    }
+
+    return (
+        <div className="myform">
+            <h2>Sign Up</h2>
+            <form onSubmit={submitForm}>
+               
+                <label htmlFor="name">Name:</label>
+                <input type="text" name="name" onChange={onChangeInput} placeholder="Your name" id="name" value={formData.name}  />
+               
+               
+               
+                <label htmlFor="email">Email:</label>
+                <input type="email" name="email" onChange={onChangeInput} placeholder="Your email" id="email" value={formData.email}  />
+                
+                
+                <label htmlFor="password">Password:</label>
+                <input type="password" name="password" onChange={onChangeInput} placeholder="New password" id="password" value={formData.password}  />
+               
+                <label htmlFor="department">department:</label>
+                <input type="text" name="department" onChange={onChangeInput} placeholder="New department" id="department" value={formData.department}  />
+                
+                
+                {successMsg && <div className="success-msg">{successMsg}</div>}
+                {errMsg && <div className="err-msg">{errMsg}</div>}
+               
+               
+                <button type="submit" disabled={wait}>Sign Up</button>
+                <div className="bottom-link"><Link to="/login">Login</Link></div>
+            </form>
+        </div>
+    )
+}
+
+export default Register;
